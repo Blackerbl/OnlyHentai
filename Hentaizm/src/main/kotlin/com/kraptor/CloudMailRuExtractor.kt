a77563b753158f278df616747aaaff8dc1a5a6be
Blackerbl <kaymazy3@gmail.com>
2026-01-26 20:21:11 +0300

    Fix threading in Hentaizm extractors

--- Hentaizm/src/main/kotlin/com/kraptor/CloudMailRuExtractor.kt
+++ Hentaizm/src/main/kotlin/com/kraptor/CloudMailRuExtractor.kt
@@ -1,4 +1,3 @@
-// ! Bu araç @Kraptor123 tarafından | @Cs-GizliKeyif için yazılmıştır.
 package com.kraptor
 
 import com.lagradost.cloudstream3.SubtitleFile
@@ -10,13 +9,15 @@ import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.INFER_TYPE
 import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.utils.newExtractorLink
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 open class CloudMailRu : ExtractorApi() {
     override val name            = "CloudMailRu"
     override val mainUrl         = "https://cloud.mail.ru"
     override val requiresReferer = false
 
-    override suspend fun getUrl(url: String, referer: String?, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit) {
+    override suspend fun getUrl(url: String, referer: String?, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit) = withContext(Dispatchers.IO) {
         val headers = mapOf(
             "Accept" to "*/*",
             "Connection" to "keep-alive",
@@ -28,7 +29,7 @@ open class CloudMailRu : ExtractorApi() {
         )
         val vidId      = url.substringAfter("public/").toByteArray()
         val vidIdEnc   = base64Encode(vidId)
-        val videoReq   = app.get(url, headers=headers).text
+        val videoReq   = app.get(url, headers = headers).text
         val regex      = Regex(pattern = "videowl_view\":\\{\"count\":\"1\",\"url\":\"([^\"]*)\"\\}", options = setOf(RegexOption.IGNORE_CASE))
         val videoMatch = regex.find(videoReq)?.groupValues?.get(1).toString()
         val videoUrl   = "$videoMatch/0p/$vidIdEnc.m3u8?double_encode=1"
@@ -36,8 +37,8 @@ open class CloudMailRu : ExtractorApi() {
 
         callback.invoke(
             newExtractorLink(
-                source  = this.name,
-                name    = this.name,
+                source  = this@CloudMailRu.name,
+                name    = this@CloudMailRu.name,
                 url     = videoUrl,
                 type    = INFER_TYPE
             ) {
